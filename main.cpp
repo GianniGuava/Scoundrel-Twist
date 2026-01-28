d6babbf4e626968705baee561a9d79a5501a3565
Giancarlo Guevarra <guevarra1006@gmail.com>
2026-01-27 22:46:32 -0800

    double check

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <random>
 #include <string>
 #include <vector>
-#include <stack>
+#include <deque>
 
 class card{
     int rank;
@@ -25,7 +25,7 @@ class card{
 
 class deck{
     std::vector<card> unshuffled_deck; 
-    std::stack<card> shuffled_deck;
+    std::deque<card> shuffled_deck;
 
     std::string suits[4] = {"Spades", "Clubs", "Hearts", "Diamonds"};  
     int ranks[13] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
@@ -44,7 +44,7 @@ class deck{
     }
 
     public:
-    void create_draw_deck(std::vector<std::string> exclude_suits, std::vector<int> exclude_ranks){
+    void become_draw_deck(std::vector<card> exclude_cards){
         create_unshuffled_deck();
 
         auto rng = std::default_random_engine{};
@@ -54,19 +54,61 @@ class deck{
             card temp = unshuffled_deck.back();
             unshuffled_deck.pop_back();
 
-            if(std::count(exclude_ranks.begin()))
+            for(card playing_card : exclude_cards){
+                //If the card that has been popped does not equal ANY of the exclusion cards:
+                if(!((temp.get_rank() == playing_card.get_rank()) && 
+                      temp.get_suit() == playing_card.get_suit())){
+                    shuffled_deck.push_front(temp);
+                }
+            }
         }
-    }
-
-    deck(std::stack<card> sd){
-        shuffled_deck = sd;
     }
 };
 
 class player{
+    int health = 20;
+    int weapon = 0;
 
+    // Metrics for later
+    // int kills = 0;
+    // int times_ran_away = 0;
+    // int times_hit = 0;
+
+    public:
+
+    void attack_bare_handed(int damage){
+        health -= damage;
+    }
+
+    void attack_weapon(int damage, int weapon){
+        damage -= weapon;
+        if(damage < 0){ damage = 0; }
+        health -= damage;
+    }
 };
 
 int main(){
+    //Default generated set. Can use user input in the future or in settings
+    std::vector<std::string> default_exclude_suits = {"Diamonds", "Hearts"};
+    std::vector<int> default_exclude_ranks = {11, 12, 13, 14};
+    std::vector<card> default_exclude_cards;
+
+    for(std::string suit : default_exclude_suits){
+        for(int rank : default_exclude_ranks){
+            card temp(rank, suit);
+            default_exclude_cards.push_back(temp);
+        }
+    }
+
+    //The Draw Deck
+    deck draw_deck;
+    draw_deck.become_draw_deck(default_exclude_cards);
+
+    //The Hand Deck
+    deck hand_deck;
+
+    //The Discard Deck
+    deck discard_deck;
+
     return 0;
 };
